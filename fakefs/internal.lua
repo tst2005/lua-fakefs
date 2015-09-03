a4a82a442a8d003864aba181883c6d682f75e35b
TsT <tst2005@gmail.com>
2015-09-04 01:08:18 +0200

    fake io.open

--- fakefs/internal.lua
+++ fakefs/internal.lua
@@ -25,36 +25,36 @@ local inodes = {
 		name = "file.log",
 		type = "f",
 		uid = 0, gid = 0,
-		mod = "rw-rw-rw-"
+		mod = "rw-rw-rw-",
 	},
 }
 
-local by_path = {
+local tree = {
 	["/"] = false,
 	["/var"] = inodes[1],
 	["/var/log"] = inodes[2],
 	["/var/log/file"] = inodes[3],
 }
+local fdata = {
+	inodes[3] = "hello world\n",
+}
 ]]--
+
+local fdata = {}
+
+--local inodes
+--local by_inodes = { [123] = inodes[1], }
+--local function make_indexes()
+--	for i, item in ipairs(inodes) do by_inodes[assert(item.ino)] = item end
+--end
+
 
 -- the catalog
 local tree = {}
 
--- inodes
-local inodes
-
---[[
-local by_inodes = { [123] = inodes[1], }
-
-local function make_indexes()
-	for i, item in ipairs(inodes) do by_inodes[assert(item.ino)] = item end
-end
-]]--
-
-
-local inodecnt
+local inodecnt = 0
 local function newinode()
-	inodecnt = inodecntinodecnt +1
+	inodecnt = inodecnt +1
 	return inodecnt
 end
 
@@ -96,7 +96,7 @@ end
 --[[
 local function filepath2inode(filepath)
 	local target = cleanpath(filepath)
-	local found = by_path[target]
+	local found = tree[target]
 	if not found then
 		return nil
 	end
@@ -107,7 +107,7 @@ M.filepath2inode = filepath2inode
 
 local function inode_info(inode, aname)
 	local target = cleanpath(filepath)
-	local found = by_path[target]
+	local found = tree[target]
 	if aname then
 		return found[aname]
 	end
@@ -168,20 +168,134 @@ end
 M.lfs_inode_info = lfs_inode_info
 
 
-function M.mkdir(path)
+function M.mkdir(path, mode)
+	mode = mode or 511
+	
 	return 
 end
 
-function M.mkfile(path)
+function M.mkfifo(path, mode)
+	mode = mode or 511
+end
+
+function M.touch(path, mode, mtime) -- mkfile
+end
+
+local fileclass = {}
+function fileclass:read()
+	assert(self.opened)
+	assert(self.readable)
+	local data = fdata[self.obj]
+	return data
+end
+function fileclass:write(data)
+	assert(self.opened)
+	assert(self.writable)
+	local obj = self.obj
+	fdata[obj] = (fdata[obj] or "") .. data
+
+	return self
+end
+function fileclass:seek(...)
+	assert(self.opened)
+	error("not implemented yet", 2)
+end
+
+function fileclass:close()
+	if not self.opened then return false end
+	-- sync ?
+	self.opened = nil
+	return true
+end
+
+
+function M.open(path, mode)
+	mode = mode or "r"
+	path = cleanpath(path)
+	local found = tree[path]
+
+	local fd = setmetatable({}, {__index = fileclass})
+	fd.readable = false
+	fd.writable = false
+	fd.opened = true
+
+	if not found then
+		if case == "r" then
+			return nil, "no such file"
+		end
+
+		found = {
+			type = "f",
+			uid = 0, gid = 0,
+			ino = newinode(),
+		}
+		tree[path] = found
+	end
+	fd.obj = found
+	if mode == "r" then
+		fd.readable = true
+	elseif mode == "w" then
+		fd.writable = true
+	end
+	return fd
+end
+
+function M.link(...) -- create hardlink/symlink
+end
+
+function M.chmod(path, mode)
 
 end
 
-function M.cd(subpath)
+function M.umask(mode) -- for file creation
+end
+
+--[[
+ access (path[, mode="f"])
+    Check real user's permissions for a file.
+    Parameters:
+
+        path string file to act on
+        mode string can contain 'r','w','x' and 'f' (default "f")
+
+    Returns:
+        int 0, if successful 
+]]--
+
+
+function M.chdir(subpath)
 	PWD = subpath
 end
+
+-- chown (path, uid, gid) 
+
+-- open ?
+-- close(fd)
+-- fdatasync
+-- fsync
 
 function M.pwd(path)
 	return PWD and PWD == "" and "/" or PWD or "/"
 end
+
+function M.stat(path) -- info about file/dir (follow the symlink target)
+end
+
+function M.lstat(path) -- info about file/dir or the symlink it-self
+end
+
+function M.readlink(path) -- return the symlink target
+end
+
+function M.realpath(path)
+end
+
+M.stdout = require"io".stdout
+M.stdin  = require"io".stdin
+M.stderr = require"io".stderr
+
+
+--M.tree = tree
+--M.data = fdata
 
 return M
--- test.fakefs.lua
+++ test.fakefs.lua
@@ -2,6 +2,18 @@ local internal = require "fakefs.internal"
 
 internal.init() -- create the rootdir
 
+for _, modname in ipairs{ "io", "fakefs.internal" } do
+	local io = require(modname)
+	print("- try with "..modname..":")
+
+	local fd = io.open("a", "w")
+	fd:write("hello world\n")
+	fd:close()
+
+	local fd = io.open("a", "r")
+	io.stdout:write( fd:read("*all") )
+	fd:close()
+end
 
 
 
